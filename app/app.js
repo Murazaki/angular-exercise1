23381354827d077fde357803dc7196d9f74b2293
Mehdi Zakaria Benadel <mehdi.benadel@gmail.com>
2015-11-06 17:47:15 +0100

    Add Task

--- app/app.js
+++ app/app.js
@@ -3,5 +3,13 @@ var app = angular.module('angular-exercise1', []);
 app.controller('AppCtrl', function($scope) {
     
     console.log("Loading controller AppCtrl");
+    
+    $scope.newTask = "";
   
+    $scope.tasklist = [{ name: "Tache n°1", done: false },{ name: "Tache n°2", done: false }];
+  
+    $scope.addTask = function() {
+      var task = { name: $scope.newTask, done: false };
+      $scope.tasklist.push(task);
+    }
 });
