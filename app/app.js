4a0ed0f3665c48702a6f34fd6de4573aac63b69b
Mehdi Zakaria Benadel <mehdi.benadel@gmail.com>
2015-11-07 01:35:28 +0100

    Correction complète, avec gestion de la taille du viewport.
    Petit bug sur Safari à ce niveau là (unités vh,vw), à voir.

--- app/app.js
+++ app/app.js
@@ -1,15 +1,35 @@
-var app = angular.module('angular-exercise1', []);
+var app = angular.module('angular-exercise1', ['ngAnimate']);
 
 app.controller('AppCtrl', function($scope) {
     
     console.log("Loading controller AppCtrl");
     
-    $scope.newTask = "";
+    $scope.newTask = ""; // variable contenant la valeur du champ input (grâce à ng-model) 
   
-    $scope.tasklist = [{ name: "Tache n°1", done: false },{ name: "Tache n°2", done: false }];
+    $scope.tasklist = []; // initialisation de la liste de tâche
   
     $scope.addTask = function() {
+      // On crée une nouvelle tâche de nom newTask (ce qui a été tapé dans l'input text)
       var task = { name: $scope.newTask, done: false };
-      $scope.tasklist.push(task);
+      
+      $scope.tasklist.push(task); // On ajoute la nouvelle tâche dans la liste de tâche
+      
+      $scope.newTask = "";  // On efface le champ input
+    }
+    
+    $scope.removeTask = function(index) {
+      // On vérifie que l'index est bien dans les valeurs autorisées (0 <= index < taille de la liste)
+      if((index >= 0) && (index < $scope.tasklist.length)) {
+        $scope.tasklist.splice(index,1);
+      }
+    }
+    
+    // La fonction markAsDone change l'état done de la tâche
+    $scope.markAsDone = function(index) {
+      // On vérifie que l'index est bien dans les valeurs autorisées (0 <= index < taille de la liste)
+      if((index >= 0) && (index < $scope.tasklist.length)) {
+        // ici on assigne NON done à done (passe de vrai à faux, ou de faux à vrai)
+        $scope.tasklist[index].done = !$scope.tasklist[index].done;
+      }
     }
 });
